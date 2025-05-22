ffe6d7f5fa274aea0e18a3f6f931dbc4e25a78bf
Alessandrop <Alessandro.software@libero.it>
2025-05-23 00:44:02 +0200

    Archiviazione sicurity Swagger

--- Delab.Backend/Program.cs
+++ Delab.Backend/Program.cs
@@ -4,82 +4,97 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using System.Text.Json.Serialization;
 
-namespace Delab.Backend
+namespace Delab.Backend;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers()
+            .AddJsonOptions( x => x.JsonSerializerOptions.ReferenceHandler =ReferenceHandler.IgnoreCycles);
+        // Add services to the container.
+
+        //builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        //builder.Services.AddSwaggerGen();
+        
+        builder.Services.AddSwaggerGen(c =>
         {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers()
-                .AddJsonOptions( x => x.JsonSerializerOptions.ReferenceHandler =ReferenceHandler.IgnoreCycles);
-            // Add services to the container.
-
-            //builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            //builder.Services.AddSwaggerGen(c =>
-            //{
-            //    c.SwaggerDoc("V1", new OpenApiInfo { Title = "", Version = "V1" });
-            //    // Aggiunge la definizione di sicurezza per il Bearer Token
-            //    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-            //    {
-            //        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
-            //        Name = "Authorization",
-            //        In = ParameterLocation.Header,
-            //        Type = SecuritySchemeType.ApiKey,
-            //        Scheme = "Bearer"
-            //    });
-            //    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-            //     {
-            //         {
-            //             new OpenApiSecurityScheme
-            //             {
-            //                 Reference = new OpenApiReference
-            //                 {
-            //                     Type = ReferenceType.SecurityScheme,
-            //                     Id = "Bearer"
-            //                 },
-            //                 Scheme = "oauth2",
-            //                 Name = "Bearer",
-            //                 In = ParameterLocation.Header,
-            //             },
-            //             new List<string>()
-            //         }
-            //     });
-            //});
-
-            builder.Services.AddDbContext<DataContext>( x=> 
-                                    x.UseSqlServer("name=DefaultConnection" 
-                                    ,option => option.MigrationsAssembly("Delab.Backend")));
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Orders Backend", Version = "v1" });
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            //    app.UseSwaggerUI(c =>
-            //    {
-            //        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
-            //        c.OAuthClientId("swagger-client"); // Client ID registrato sul tuo auth server
-            //        c.OAuthUsePkce(); // Abilita PKCE
-            //        c.OAuthScopeSeparator(" "); // Separatore degli scope
-            //        string url= "https://localhost:7023/swagger/index.html" ;
-            //        Task(()=>OpenBrowser(url));
-            //    });
-            //}
+                Description = @"JWT Authorization header using the Bearer scheme. <br /> <br />
+                      Enter 'Bearer' [space] and then your token in the text input below.<br /> <br />
+                      Example: 'Bearer 12345abcdef'<br /> <br />",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+          {
+            {
+              new OpenApiSecurityScheme
+              {
+                Reference = new OpenApiReference
+                  {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                  },
+                  Scheme = "oauth2",
+                  Name = "Bearer",
+                  In = ParameterLocation.Header,
+                },
+                new List<string>()
+              }
+            });
+        });
 
-            app.UseHttpsRedirection();
+        builder.Services.AddDbContext<DataContext>( x=> 
+                                x.UseSqlServer("name=DefaultConnection" 
+                                ,option => option.MigrationsAssembly("Delab.Backend")));
 
-            app.UseAuthorization();
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
+                
+            //string url = "https://localhost:7023/swagger/index.html";
+            //Task.Run(() => OpenBrowser(url));
+            
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
 
 
-            app.MapControllers();
+        app.MapControllers();
 
-            app.Run();
+        app.Run();
+    }
+    // Método para abrir el navegador
+    static void OpenBrowser(string url)
+    {
+        try
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo
+            {
+                FileName = url,
+                UseShellExecute = true
+            };
+            System.Diagnostics.Process.Start(psi);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error al abrir el navegador: {ex.Message}");
         }
     }
 }
+
---
+++ Delab.Shared/Enum/TypeUser.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Delab.Shared.Enum;
+
+public enum TypeUser
+{
+    Admin = 1,
+    OpInt = 2,
+    Cliente = 3,
+    Fornitore = 4,
+    Visitatore = 5
+
+}
+
